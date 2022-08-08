7ee0c091d973c57a30d8c7950dd5fd60c6610fb2
petebeegle <pete.beegle@gmail.com>
2022-08-08 11:44:44 -0400

    Install starship and omz if not found

--- omz/install.sh
+++ omz/install.sh
@@ -2,5 +2,10 @@
 
 set -e
 
+if [ ! -d "${HOME}/.oh-my-zsh" ]; then
+  echo "🚀 Installing dependency: oh-my-zsh"
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
 ln -sf "${DOTFILES_LOCATION}/omz/aliases.zsh" "${HOME}/.oh-my-zsh/custom/aliases.zsh"
 ln -sf "${DOTFILES_LOCATION}/omz/exports.zsh" "${HOME}/.oh-my-zsh/custom/exports.zsh"
--- starship/install.sh
+++ starship/install.sh
@@ -2,4 +2,9 @@
 
 set -e
 
+if [ ! -x "$(command -v starship)" ]; then
+  echo "🚀 Installing dependency: starship"
+  curl -sS https://starship.rs/install.sh | sh
+fi
+
 ln -sf "${DOTFILES_LOCATION}/starship/starship.toml" "${HOME}/.config/starship.toml"
