07785e3be2cb5a2c2cfcd9c65045c08d57bda0a2
petebeegle <pete.beegle@gmail.com>
2022-08-08 11:27:33 -0400

    Add extensions list for vscode

--- vscode/install.sh
+++ vscode/install.sh
@@ -3,3 +3,11 @@
 set -e
 
 ln -sf "${DOTFILES_LOCATION}/vscode/settings.json" "${HOME}/.vscode-server/data/Machine/settings.json"
+
+CODE_EXTENSIONS=(
+  Tyriar.theme-sapphire
+  esbenp.prettier-vscode
+  foxundermoon.shell-format
+  eamodio.gitlens
+)
+for ext in "${CODE_EXTENSIONS[@]}"; do printf "  🚀 Installing extension: %s" "${ext}" && code --install-extension "${ext}" --force; done
