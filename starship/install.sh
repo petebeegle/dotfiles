59751fdba44b24a04b43a8a7eec6890693e24b03
Pete Beegle <petebeegle@users.noreply.github.com>
2022-11-26 05:06:17 +0000

    bypass install prompt for starship

--- starship/install.sh
+++ starship/install.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ ! -x "$(command -v starship)" ]; then
   echo "🚀 Installing dependency: starship"
-  curl -sS https://starship.rs/install.sh | sh
+  curl -sS https://starship.rs/install.sh | sh -s -- -f
 fi
 
 ln -sf "${DOTFILES_LOCATION}/starship/starship.toml" "${HOME}/.config/starship.toml"
