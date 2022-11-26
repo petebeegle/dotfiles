e7a5376e72ec1c7fba6f3b73a9929d89b2fe109a
Pete Beegle <petebeegle@users.noreply.github.com>
2022-11-26 04:52:16 +0000

    remove remote_container check for starship

--- starship/install.sh
+++ starship/install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ ! -x "$(command -v starship)" ] && [ -z ${REMOTE_CONTAINERS+x}]; then
+if [ ! -x "$(command -v starship)" ]; then
   echo "🚀 Installing dependency: starship"
   curl -sS https://starship.rs/install.sh | sh
 fi
