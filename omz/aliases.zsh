3a026e6ce888070a91b4d4c43491c8e2fef764c6
Pete Beegle <peter.beegle@fanduel.com>
2024-09-24 16:32:54 -0400

    Add docker-compose aliases

--- omz/aliases.zsh
+++ omz/aliases.zsh
@@ -1,1 +1,6 @@
 alias awslocal="AWS_ACCESS_KEY_ID=test AWS_SECRET_ACCESS_KEY=test AWS_DEFAULT_REGION=${DEFAULT_REGION:-$AWS_DEFAULT_REGION} aws --endpoint-url=http://${LOCALSTACK_HOST:-localhost}:4566"
+
+# docker compose aliases
+alias dcd="docker-compose down"
+alias dcu="docker-compose up"
+alias dcud="docker-compose up -d"
