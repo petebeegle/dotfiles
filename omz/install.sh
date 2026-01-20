3a8d48b80dbc53a739930a31952c3b1dd28aec0f
Pete Beegle <peter.beegle@fanduel.com>
2026-01-20 15:47:21 -0500

    Fix missing environment variable

--- omz/install.sh
+++ omz/install.sh
@@ -6,6 +6,3 @@ if [ ! -d "${HOME}/.oh-my-zsh" ]; then
   echo "🚀 Installing dependency: oh-my-zsh"
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
-
-ln -sf "${DOTFILES_LOCATION}/omz/aliases.zsh" "${HOME}/.oh-my-zsh/custom/aliases.zsh"
-ln -sf "${DOTFILES_LOCATION}/omz/exports.zsh" "${HOME}/.oh-my-zsh/custom/exports.zsh"
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -2,3 +2,6 @@ export PATH="$PATH:$HOME/.rvm/bin"
 export PATH="/$HOME/.rd/bin:$PATH"
 export SDKMAN_DIR="$HOME/.sdkman"
 export NVM_DIR="$HOME/.nvm"
+
+# Ensure dotfiles location is available for .zshrc sourcing.
+export DOTFILES_LOCATION="${DOTFILES_LOCATION:-$HOME/code/petebeegle/dotfiles}"
