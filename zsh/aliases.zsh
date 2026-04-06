bd88ffd87db9a10d57a01f6ae3697138a2396a21
Pete Beegle <peter.beegle@fanduel.com>
2026-04-06 16:11:02 -0400

    Add function to get current branch

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -78,6 +78,18 @@ function git_main_branch() {
   return 1
 }
 
+# Get the current branch name or fallback to short SHA
+function git_current_branch() {
+  local ref
+  ref=$(command git symbolic-ref --quiet HEAD 2>/dev/null)
+  local ret=$?
+  if [[ $ret != 0 ]]; then
+    [[ $ret == 128 ]] && return
+    ref=$(command git rev-parse --short HEAD 2>/dev/null) || return
+  fi
+  echo ${ref#refs/heads/}
+}
+
 function grename() {
   if [[ -z "$1" || -z "$2" ]]; then
     echo "Usage: $0 old_branch new_branch"
